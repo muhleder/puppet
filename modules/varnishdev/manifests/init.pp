ffd75804beb768a4da36fa046a62648b1cc35a87
Mark Muhleder <mark@analogweb.co.uk>
2012-01-20 21:52:55 +0000

    Install varnish from ubuntu repo
    Critical error fixed upstream
    
    https://bugs.launchpad.net/ubuntu/+source/varnish/+bug/793914
    Signed-off-by: Mark Muhleder <mark@analogweb.co.uk>

--- modules/varnishdev/manifests/init.pp
+++ modules/varnishdev/manifests/init.pp
@@ -1,21 +1,9 @@
 class varnishdev {
 
-  aptkey { "varnish":
-    ensure => present,
-    apt_key_url => 'http://repo.varnish-cache.org/debian/GPG-key.txt',
-  }
-
-  exec { 'varnish-add-sources':
-    unless => '/bin/grep "^deb http://repo.varnish-cache.org/ubuntu/ $(lsb_release -s -c)" /etc/apt/sources.list',
-    command => '/bin/echo "deb http://repo.varnish-cache.org/ubuntu/ $(lsb_release -s -c) varnish-2.1" >> /etc/apt/sources.list',
-    require => Aptkey['varnish'],
-  }
-
   package { 'varnish' :
     require => [
       File['/etc/varnish/default.vcl'],
       File['/etc/default/varnish'],
-      Exec['varnish-add-sources'],
     ],
     ensure => 'installed',
   }
