5196acca10873e82bd8546e9da05641aec68f272
Mark Muhleder <mark@analogweb.co.uk>
2012-01-22 12:20:12 +0000

    Get apache module settings working again.
    Signed-off-by: Mark Muhleder <mark@analogweb.co.uk>

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -10,6 +10,15 @@ class apache2 {
 
   package { $packagelist: ensure => installed }
 
+  apache2::module { $modsenable:
+    ensure=> present,
+    require => Service['apache2'],
+  }
+  apache2::module { $modsdisable:
+    ensure=> absent,
+    require => Service['apache2'],
+  }
+
 
   $apache_conf_dir = "/etc/apache2"
   $php_conf_dir = "/etc/php5"
@@ -69,7 +78,25 @@ class apache2 {
     source => "puppet:///modules/apache2/etc/apache2/ports.conf",
   }
 
-
+  # Define an apache2 module. Debian packages place the module config
+  # into /etc/apache2/mods-available.
+  define module ( $ensure = 'present', $require = 'apache2' ) {
+    case $ensure {
+      'present' : {
+        exec { "/usr/sbin/a2enmod $name":
+         unless => "/bin/readlink -e ${apache2_mods}-enabled/${name}.load",
+         notify => Exec["force-reload-apache2"],
+        }
+      }
+      'absent': {
+        exec { "/usr/sbin/a2dismod $name":
+         onlyif => "/bin/readlink -e ${apache2_mods}-enabled/${name}.load",
+         notify => Exec["force-reload-apache2"],
+        }
+      }
+      default: { err ( "Unknown ensure value: '$ensure'" ) }
+    }
+  }
 
   # Notify this when apache needs a reload. This is only needed when
   # sites are added or removed, since a full restart then would be
