1687cdddfb47ad83ccee255575204ed7bec643a2
Mark Muhleder <mark@analogweb.co.uk>
2012-01-30 22:32:58 +0000

    WIP solr module
    Signed-off-by: Mark Muhleder <mark@analogweb.co.uk>

--- modules/solr/manifests/init.pp
+++ modules/solr/manifests/init.pp
@@ -1,7 +1,23 @@
 class solr {
 
-  package {'solr':
+  package {'solr-jetty':
     ensure => present,
+  }
+
+  exec {'create-drupal-solr':
+    require => Package['solr-jetty'],
+    unless => '',
+    command => 'cp -R /var/solr/example /var/solr/drupal'
+  }
+
+  file {'/var/solr/apache-solr/drupal/schema.xml':
+    require => Exec['create-drupal-solr'],
+    source => puppet:///modules/solr/var/solr/apache-solr/drupal/schema.xml
+  }
+
+  file {'/var/solr/apache-solr/drupal/solrconfig.xml':
+    require => Exec['create-drupal-solr'],
+    source => puppet:///modules/solr/var/solr/apache-solr/drupal/solrconfig.xml
   }
 
 }
