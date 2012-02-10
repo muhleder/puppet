1827f867a99b9bb47f6557b09ffd99555406259c
Mark Muhleder <mark@analogweb.co.uk>
2012-02-10 18:10:52 +0000

    Get solr module working
    Signed-off-by: Mark Muhleder <mark@analogweb.co.uk>

--- modules/solr/manifests/init.pp
+++ modules/solr/manifests/init.pp
@@ -1,6 +1,11 @@
 class solr {
 
+  package {'openjdk-6-jdk':
+    ensure => present,
+  }
+
   package {'solr-jetty':
+    require => Package['openjdk-6-jdk'],
     ensure => present,
   }
 
@@ -19,8 +24,28 @@ class solr {
     source => 'puppet:///modules/solr/etc/default/jetty',
   }
 
+  file {'/etc/init.d/solr':
+    source => 'puppet:///modules/solr/etc/init.d/solr',
+    mode => 744,
+  }
+
+  file {'/usr/share/solr':
+    ensure => directory,
+    owner => jetty,
+  }
+
+  file {'/usr/share/solr/data':
+    require => File['/usr/share/solr'],
+    ensure => directory,
+    owner => jetty,
+  }
+
   service {'jetty':
-    require => File['/etc/default/jetty'],
+    require => File[
+      '/usr/share/solr/data',
+      '/etc/default/jetty'
+    ],
+    subscribe => File['/usr/share/solr/data'],
     ensure => running,
   }
 
