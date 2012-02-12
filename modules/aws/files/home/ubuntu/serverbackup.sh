0f48083e85d21343bf3f7d26794a7f874543f190
Mark Muhleder <mark@analogweb.co.uk>
2012-02-12 01:00:06 +0000

    Clean up the server backup script
    Signed-off-by: Mark Muhleder <mark@analogweb.co.uk>

--- modules/aws/files/home/ubuntu/serverbackup.sh
+++ modules/aws/files/home/ubuntu/serverbackup.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
-
+# Confidential keys etc should be set as global environment variables in /etc/environment
+. /etc/environment
 description="server-backup-`date +%Y-%m-%d-%H-%M-%S`"
-volumeid=$AWS_VOLUME_ID
-accesskeyid=$AWS_ACCESS_KEY
-secretaccesskey=$AWS_SECRET_KEY
-region=$AWS_REGION
-
 
 ec2-consistent-snapshot  \
 --debug \
---aws-access-key-id=$accesskey \
---aws-secret-access-key=$secretaccesskey \
+--aws-access-key-id=$AWS_ACCESS_KEY \
+--aws-secret-access-key=$AWS_SECRET_KEY \
 --description=$description  \
---region=$region \
-$volumeid
+--region=$AWS_REGION \
+$AWS_VOLUME_ID
 
 
 ec2-prune-snapshots  \
---aws-access-key-id=$accesskey \
---aws-secret-access-key=$secretaccesskey \
+--aws-access-key-id=$AWS_ACCESS_KEY \
+--aws-secret-access-key=$AWS_SECRET_KEY \
 --months 0 --weeks 1 --days 1 \
 --dayofmonth 1 --dayofweek 1 \
---region=$region \
-$volumeid
+--region=$AWS_REGION \
+$AWS_VOLUME_ID
